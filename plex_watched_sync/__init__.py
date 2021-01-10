25480099f573e4f0841022887f8b200cbf61b430
Stephan Peijnik-Steinwender <speijnik@anexia-it.com>
2021-01-10 17:53:28 +0100

    Bump version for re-release
    Signed-off-by: Stephan Peijnik-Steinwender <speijnik@anexia-it.com>

--- plex_watched_sync/__init__.py
+++ plex_watched_sync/__init__.py
@@ -2,4 +2,4 @@
 """plex_status_sync provides logic and CLI entrypoint which implement
 synchronization of the "watched" state of from one Plex server to another
 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
